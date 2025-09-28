b730871e4371eabe30f4d6873e1298ce413df97c
Thiago Torres <thiago.torres@outlook.com.br>
2025-09-28 18:00:08 -0300

    Add index page and web route

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,11 @@ db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
-
     app.config.from_pyfile('config.py', silent=True)
-
     db.init_app(app)
+
+    from app.routes.web import web_bp
+    app.register_blueprint(web_bp)
 
     with app.app_context():
         db.create_all()
