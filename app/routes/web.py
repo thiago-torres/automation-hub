860dad3a7a05486b4cce06e224b66dd1d0038360
Thiago Torres <thiago.torres@outlook.com.br>
2025-09-30 09:35:14 -0300

    Initiate 404 route and page

--- app/routes/web.py
+++ app/routes/web.py
@@ -5,3 +5,7 @@ web_bp = Blueprint('web', __name__)
 @web_bp.route('/')
 def index():
     return render_template('index.html')
+
+@web_bp.app_errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
